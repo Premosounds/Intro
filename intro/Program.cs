6e5debc00159f9fa87bc0829b520803d9d26d17e
Premosounds <77034488+Premosounds@users.noreply.github.com>
2021-03-11 11:00:30 +0700

    Add files via upload

---
+++ intro/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        // access_level  return_type  method_name  (params)
+        public int Variables()
+        {
+            string name = "Jay";
+            int age = 32;
+            float distance = 3445.423f
+            decimal price = 29.99m;
+            bool found = false;
+
+            return 1;
+
+        }
+    }
+}
+
+
+
+// JS = pascalCase
+// CS = CamelCase
+
+
